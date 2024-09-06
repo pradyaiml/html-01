59b11e55aa0fd37bc59a80258a6274169b5df7e4
pradyaiml <pradyaiml@gmail.com>
2024-09-06 18:55:33 +0530

    adding more files

---
+++ objectdemo.js
@@ -0,0 +1,13 @@
+let product = {
+    name: "Apple Iphone 11 Pro max (512 GB, Blue)",
+    price: 150000,
+    discount: 15,
+    isAvailable: false
+};
+
+let socialHandles = {
+    fb: 9000,
+    insta: 6700,
+    "linkedin": 2000,
+    x: 5000
+};
